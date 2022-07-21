df05f64fbdb2582bacea0e8dc2d793fa6f6e543c
qdeca <quentin.decayeux@ogury.co>
2022-07-21 22:48:03 +0200

    changes

--- exercises/data-aggregation/load.py
+++ exercises/data-aggregation/load.py
@@ -3,7 +3,7 @@ import psycopg2
 
 def main():
     connection = psycopg2.connect(user='postgres', password='postgres',
-                                  host='default-db.cfcjvgyonsqr.eu-west-3.rds.amazonaws.com', port=5432)
+                                  host='prod-rds-db.cfcjvgyonsqr.eu-west-3.rds.amazonaws.com', port=5432)
     cursor = connection.cursor()
     with open("init_museum_table.sql", "r") as sql_file :
         cursor.execute(sql_file.read())
